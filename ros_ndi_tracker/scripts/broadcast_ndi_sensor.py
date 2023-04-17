efde3c1abeba38dc56e2c7e8ad357bad6eded1e7
jsbyysheng <jsbyysheng@gmail.com>
2023-04-18 01:47:33 +0800

    sync

--- ros_ndi_tracker/scripts/broadcast_ndi_sensor.py
+++ ros_ndi_tracker/scripts/broadcast_ndi_sensor.py
@@ -6,15 +6,15 @@ import sys
 import tf_conversions
 import tf2_ros
 from geometry_msgs.msg import TransformStamped, Pose
-from ctl_msgs.msg import Aurora
+from ros_ndi_tracker.msg import NDISensor
 from roscpp.srv import SetLoggerLevel
 from sksurgerynditracker.nditracker import NDITracker
 
 
 class NDI_Sensor:
     def __init__(self) -> None:
-        self._tag = self.__class__.__name__
-        rospy.init_node('ndi_sensor_node')
+        rospy.init_node('ndi_sensor_node', anonymous=True)
+        self._tag = rospy.get_name()
 
         self._DEBUG = bool(rospy.get_param(f"{rospy.get_name()}/DEBUG_OUTPUT"))
         if self._DEBUG:
@@ -22,17 +22,30 @@ class NDI_Sensor:
             s = rospy.ServiceProxy(f"{rospy.get_name()}/set_logger_level", SetLoggerLevel)
             s.call('rosout', 'DEBUG')
 
+        self._tracker_type = rospy.get_param(f"{rospy.get_name()}/tracker_type")
+        self._port = rospy.get_param(f"{rospy.get_name()}/port")
         self._base_link_name = rospy.get_param(f"{rospy.get_name()}/base_link_name")
-        markers = rospy.get_param(f"{rospy.get_name()}/ndi_sensor/marker")
+
         self._markers = {}
-        for marker in markers:
-            self._markers[marker['id']] = marker['name']
+
+        if self._tracker_type == 'aurora':
+            markers = rospy.get_param(f"{rospy.get_name()}/aurora_sensor/marker")
+            
+            for marker in markers:
+                self._markers[marker['id']] = marker['name']
+        else:
+            pass
 
         self._br = tf2_ros.TransformBroadcaster()
-        self._pub = rospy.Publisher(f"{rospy.get_name()}/sensor_data", Aurora, queue_size=10)
-        self._aurora_sensor_msg = Aurora()
+        self._pub = rospy.Publisher(f"{rospy.get_name()}/sensor_data", NDISensor, queue_size=10)
+        self._ndi_sensor_msg = NDISensor()
 
         rospy.loginfo(f"{self._tag} - __init__()")
+        rospy.loginfo(f"{self._tag} - \
+                      Tracker Type: {self._tracker_type}, \
+                      Port: {self._port}, \
+                      Base Link: {self._base_link_name} \
+                      N Markers: {len(self._markers)}")
 
         rospy.on_shutdown(self._shutdown)
 
@@ -50,19 +63,19 @@ class NDI_Sensor:
         for idx in range(len(port_handles)):
             if math.isnan(tracking[idx, 0, 0]) or timestamps[idx] == 0.0:
                 continue
-            self._aurora_sensor_msg.port_handles[idx] = port_handles[idx]
+            self._ndi_sensor_msg.port_handles[idx] = port_handles[idx]
             if port_handles[idx] in self._markers:
-                self._aurora_sensor_msg.marker_names[idx] = self._markers[port_handles[idx]]
+                self._ndi_sensor_msg.marker_names[idx] = self._markers[port_handles[idx]]
             else:
-                self._aurora_sensor_msg.marker_names[idx] = f"marker_{idx}"
-            self._aurora_sensor_msg.timestamps[idx] = timestamps[idx]
-            self._aurora_sensor_msg.framenumbers[idx] = framenumbers[idx]
-            self._aurora_sensor_msg.quality[idx] = quality[idx]
+                self._ndi_sensor_msg.marker_names[idx] = f"marker_{idx}"
+            self._ndi_sensor_msg.timestamps[idx] = timestamps[idx]
+            self._ndi_sensor_msg.framenumbers[idx] = framenumbers[idx]
+            self._ndi_sensor_msg.quality[idx] = quality[idx]
 
             t = TransformStamped()
             t.header.stamp = rospy.Time.from_sec(timestamps[idx])
             t.header.frame_id = self._base_link_name
-            t.child_frame_id = self._aurora_sensor_msg.marker_names[idx]
+            t.child_frame_id = self._ndi_sensor_msg.marker_names[idx]
             t.transform.translation.x = tracking[idx, 0, 3]
             t.transform.translation.y = tracking[idx, 1, 3]
             t.transform.translation.z = tracking[idx, 2, 3]
@@ -81,38 +94,38 @@ class NDI_Sensor:
             p.orientation.x = t.transform.rotation.x
             p.orientation.y = t.transform.rotation.y
             p.orientation.z = t.transform.rotation.z
-            self._aurora_sensor_msg.tracking[idx] = p
+            self._ndi_sensor_msg.tracking[idx] = p
 
         return trans
 
     def execute(self):
         SETTINGS = {
-            "tracker type": "aurora",
-            "serial port": "/dev/ttyUSB0",
+            "tracker type": self._tracker_type,
+            "serial port": self._port,
         }
         rospy.loginfo(f"{self._tag} - Create NDITracker object.")
         self.TRACKER = NDITracker(SETTINGS)
-        rospy.loginfo(f"{self._tag} - Start Aurora Tracking.")
+        rospy.loginfo(f"{self._tag} - Start NDI {self._tracker_type} Tracking.")
         self.TRACKER.start_tracking()
-        rospy.loginfo(f"{self._tag} - Aurora Tracking is ready.")
+        rospy.loginfo(f"{self._tag} - NDI {self._tracker_type} Tracking is ready.")
 
         port_handles, timestamps, framenumbers, tracking, quality = self.TRACKER.get_frame()
         n_tools = len(port_handles)
-        self._aurora_sensor_msg.port_handles = [0] * n_tools
-        self._aurora_sensor_msg.marker_names = ['marker'] * n_tools
-        self._aurora_sensor_msg.timestamps = [0.0] * n_tools
-        self._aurora_sensor_msg.framenumbers = [0] * n_tools
-        self._aurora_sensor_msg.tracking = [Pose()] * n_tools
-        self._aurora_sensor_msg.quality = [0.0] * n_tools
+        self._ndi_sensor_msg.port_handles = [0] * n_tools
+        self._ndi_sensor_msg.marker_names = ['marker'] * n_tools
+        self._ndi_sensor_msg.timestamps = [0.0] * n_tools
+        self._ndi_sensor_msg.framenumbers = [0] * n_tools
+        self._ndi_sensor_msg.tracking = [Pose()] * n_tools
+        self._ndi_sensor_msg.quality = [0.0] * n_tools
 
         while not rospy.is_shutdown():
             try:
                 self._br.sendTransform(self._receive_sensor_data())
-                self._pub.publish(self._aurora_sensor_msg)
+                self._pub.publish(self._ndi_sensor_msg)
             except Exception as e:
                 rospy.logerr(f"{self._tag} - Cannot read from the device: {e}")
 
 
 if __name__ == '__main__':
-    sensor = Aurora_Sensor()
+    sensor = NDI_Sensor()
     sensor.execute()
